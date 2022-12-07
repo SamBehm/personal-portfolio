3e51cb236d27922d048f55ad91c9c9091e03240f
Sam Behm <sambehmwork@gmail.com>
2022-12-07 23:32:22 +1000

    added resizing capability to canvas on window resize

--- portfolio/main.js
+++ portfolio/main.js
@@ -201,3 +201,15 @@ function animate() {
   // controls.update();
   renderer.render(scene, camera);
 }
+
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
